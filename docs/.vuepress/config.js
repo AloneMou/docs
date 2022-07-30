615fc78087935a7ef9602420d850134a3784649c
AgoniMou <2627045617@qq.com>
2022-07-30 14:05:27 +0800

    add 初始化在线文档

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,43 @@
+// const {defaultTheme} = require("vuepress");
+const {backToTopPlugin} = require("@vuepress/plugin-back-to-top");
+const {registerComponentsPlugin} = require("@vuepress/plugin-register-components");
+const {nprogressPlugin} = require("@vuepress/plugin-nprogress");
+const {searchPlugin} = require("@vuepress/plugin-search");
+const {viteBundler} = require('@vuepress/bundler-vite')
+// const {theme} = require("./theme");
+const {hopeTheme} = require("vuepress-theme-hope");
+
+module.exports = {
+    title: 'AA',
+    plugins: [
+        backToTopPlugin(),
+        registerComponentsPlugin({}),
+        nprogressPlugin(),
+        searchPlugin({
+            '/': {
+                placeholder: 'Search',
+            },
+        }),
+    ],
+    bundler: viteBundler({
+        viteOptions: {
+            build: {
+                assetsDir: 'static',
+                outDir: '/dist'
+            },
+        },
+        vuePluginOptions: {},
+    }),
+    theme: hopeTheme({
+        author: {
+            name: 'AgoniMou',
+            url: 'https://mrhope.site',
+        },
+        iconAssets: '//at.alicdn.com/t/font_2410206_a0xb9hku9iu.css',
+        logo: '/logo.svg',
+        repo: 'vuepress-theme-hope/vuepress-theme-hope',
+        docsDir: 'demo/src',
+        pageInfo: ['Author', 'Original', 'Date', 'Category', 'Tag', 'ReadingTime'],
+    }),
+    shouldPrefetch: false,
+}
---
+++ docs/docker/README.md
@@ -0,0 +1,14 @@
+# Docker入门安装
+
+## 项目介绍
+
+学习教程推荐菜鸟教程: [https://www.runoob.com/docker](https://www.runoob.com/docker)
+真好笑！ :joy:
+
+
+Here's a simple footnote,[^1] and here's a longer one.[^bignote]
+
+[^1]: This is the first footnote.
+
+[^bignote]: Here's one with multiple paragraphs and code.
+
